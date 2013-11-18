f6d27160bc6f3dc817f941f2f4272a72b3252d5a
vsechko <vsechko@gmail.com>
2013-11-18 08:50:11 +0200

    Picture

--- widerselectie8.js
+++ widerselectie8.js
@@ -1,14 +1,22 @@
-var el;
-
-$("select")
-  .each(function() {
-    el = $(this);
-    el.data("origWidth", el.outerWidth()) // IE 8 can haz padding
-  })
-  .mouseenter(function(){
-    $(this).css("width", "auto");
-  })
-  .bind("blur change", function(){
-    el = $(this);
-    el.css("width", el.data("origWidth"));
-  });
+<!--[if lt IE 9]>
+	<script>
+		$(function() {
+		
+			var el;
+			
+			$("select.fix-me")
+				.each(function() {
+					el = $(this);
+					el.data("origWidth", el.outerWidth()) // IE 8 will take padding on selects
+				})
+			  .mouseenter(function(){
+			    $(this).css("width", "auto");
+			  })
+			  .bind("blur change", function(){
+			  	el = $(this);
+			    el.css("width", el.data("origWidth"));
+			  });
+		
+		});
+	</script>
+	<![endif]-->
